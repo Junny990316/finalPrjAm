f2b641c42bcd11d1ab9aa8ab141a786e8bab86b3
Junny990316 <heojueun0316@gmail.com>
2023-06-08 12:39:32 +0900

    jpa / test 환경 구축

--- src/main/java/com/kh/finalPrjAm/repository/ItemRepository.java
+++ src/main/java/com/kh/finalPrjAm/repository/ItemRepository.java
@@ -3,8 +3,19 @@ package com.kh.finalPrjAm.repository;
 import com.kh.finalPrjAm.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 //JpaRepositoty는 첫번째는 엔티티 타입 클래스를 넣어주고, 두번째는 기본 키
 // 기본적인 CRUD 및 페이징 처리를 위한 메소드는 정의 되어 있음
 public interface ItemRepository extends JpaRepository<Item, Long> {
 
+    List<Item> findByItemNm(String itemNm);
+    // or 조건 처리하기
+    List<Item> findByItemNmOrItemDetail(String itemNm, String itemDetail);
+
+    // LessThan : 매개변수로 전달 받은 값보다 작은 상품 조회
+    List<Item> findByPriceLessThan(Integer price);
+
+    // OrderBy 로 정렬 : OrderBy + 속성명 + Asc or Desc
+    List<Item> findByPriceLessThanOrderByPriceDesc(Integer price);
 }

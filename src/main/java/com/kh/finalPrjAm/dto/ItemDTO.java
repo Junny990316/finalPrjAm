2e5c7a890551939fbaa637453c042deeb73eb0c0
Junny990316 <heojueun0316@gmail.com>
2023-06-16 11:38:25 +0900

    thymeleaf 만들기

--- src/main/java/com/kh/finalPrjAm/dto/ItemDTO.java
+++ src/main/java/com/kh/finalPrjAm/dto/ItemDTO.java
@@ -3,9 +3,12 @@ package com.kh.finalPrjAm.dto;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Getter @Setter
 public class ItemDTO {
     String itemNm;
     int price;
     String itemDetail;
+    private LocalDateTime regTime;
 }
